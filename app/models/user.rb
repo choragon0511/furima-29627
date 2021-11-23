4546dba8380dc8609cd7fa178cdead6da3bbb919
choragon0511 <choragonmoon@gmail.com>
2021-11-24 08:50:08 +0900

    新規登録
    空の状態で登録できなくした
    空のエラーメッセージ表示するようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :first_name, :first_name_kana,
+            :last_name, :last_name_kana, :birthday, presence: true
+
 end
