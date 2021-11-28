5160af251c2f9b866f7d0c23b366ac35d1070e1b
choragon0511 <choragonmoon@gmail.com>
2021-11-29 00:17:18 +0900

    FactoryBot設定
    FactoryBotで自動で入力の設定

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Nmae.name}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name {Gimei.name.first.kanji}
+    first_name_kana {Gimei.name.first.katakana}
+    last_name {Gimei.name.last.kanji}
+    last_name_kana {Gimei.name.last.katakana}
+    birthday {Faker::Date.in_date_period}
+  end
+end
