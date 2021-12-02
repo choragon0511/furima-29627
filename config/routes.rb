eb1a96920fec63218d35380a0181915c234db98a
choragon0511 <choragonmoon@gmail.com>
2021-12-02 23:11:10 +0900

    出品画面への遷移
    フリマ素材入れて
    リンク編集
    ログインしてる時に移動
    ログインしてなかったらログイン画面に移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  
-  get 'items', to: 'items#index'
+
+  resources :items, only: [:index, :new]
   root to: 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
