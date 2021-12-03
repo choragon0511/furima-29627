f5c734da3084d22db99fd193acf7a8b687a5532b
choragon0511 <choragonmoon@gmail.com>
2021-12-03 20:35:30 +0900

    ActiveHash導入
    ActiveHash導入
    カテゴリ選択できるように設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :wait_day
 end
---
+++ app/models/wait_day.rb
@@ -0,0 +1,12 @@
+class Wait_day < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '１〜２で発送' },
+    { id: 3, name: '３〜４で発送' },
+    { id: 4, name: '４〜７で発送' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+
+  end
