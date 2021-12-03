11538fbad219de8b6b49c8da7e5bb7ef021483ef
choragon0511 <choragonmoon@gmail.com>
2021-12-03 11:27:04 +0900

    Active Storageの導入
    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,10 @@ class ItemsController < ApplicationController
   def new
   end
 
+  private
+
+  def message_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
