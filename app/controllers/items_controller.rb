25b7272fa858bf032a7bbd97bad12c7cf637ffcd
choragon0511 <choragonmoon@gmail.com>
2021-12-08 22:15:25 +0900

    商品一覧表示機能
    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    # @items = Item.all
+    # @items = Item.includes(:purchase)
+    @items = Item.all
   end
 
   def new
@@ -16,6 +17,9 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
   end
 
   private
