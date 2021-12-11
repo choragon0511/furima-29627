e66340f6155795e6a5dad2257e707c47adb7a5c8
choragon0511 <choragonmoon@gmail.com>
2021-12-11 13:21:59 +0900

    コントローラー修正
    ユーザー判別をまとめて
    edit,destroyにセット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :not_user_move, only: [:edit, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
@@ -23,9 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id != @item.user_id
-      redirect_to root_path
-    end
   end
 
   def update
@@ -52,5 +50,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def not_user_move
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
   end
 end
