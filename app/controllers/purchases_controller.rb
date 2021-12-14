33b613d8b0b537fffac3682f71b5468034ea46a9
choragon0511 <choragonmoon@gmail.com>
2021-12-14 23:40:36 +0900

    購入商品表示
    購入画面に移動で商品が表示されるようにした

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
 
   def new
+    @item = Item.find(params[:item_id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :items
-  resources :purchases, only: [:new, :create]
-  root to: 'items#index'
+  resources :items do
+    resources :purchases, only: [:new, :create]
+  end
+    root to: 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
