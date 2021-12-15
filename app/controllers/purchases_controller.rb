8d033d7d8978c5fba2216021b8ba137f148c8136
choragon0511 <choragonmoon@gmail.com>
2021-12-15 13:14:19 +0900

    購入画面制限
    ログインしてない人
    自分の出品商品で
    購入画面にいけないようにした

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,22 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :mine_move
+
 
   def new
+  end
+
+  private
+
+  def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def mine_move
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
 end
